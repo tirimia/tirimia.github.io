482d8ea40d027b17483a2d253c96301bb39222b6
Theodor-Alexandru Irimia <11174371+tirimia@users.noreply.github.com>
2024-11-10 23:41:07 +0100

    Fix link

--- link-redirect.mjs
+++ link-redirect.mjs
@@ -4,7 +4,7 @@ export const remarkRewriteLinks = (options = {}) => {
     return (tree) => {
         visit(tree, 'link', (node) => {
             if (node.url && node.url.endsWith('.md')) {
-                node.url = "azubinomicon/notes/" + node.url.replace(".md", '')
+                node.url = "/azubinomicon/notes/" + node.url.replace(".md", '')
             }
         })
     }
